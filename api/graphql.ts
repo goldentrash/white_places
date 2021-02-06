47c78c1471885342516ad41366d941f1d3067630
goldentrash <startoflateness@gmail.com>
2021-02-06 14:34:11 +0900

    fix graphql

--- api/graphql.ts
+++ api/graphql.ts
@@ -2,7 +2,7 @@ import { helper } from './helper';
 
 export const handler = async (event: any, context: any) => {
 	return {
-		stattusCode: 200,
-		body: `Hello ${helper(1)}`,
+		statusCode: 200,
+		body: JSON.stringify({ event, context }),
 	};
 };
