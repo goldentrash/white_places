631ff24b9ee68fbc5b6ff9ff0b2bec6608ee71ad
goldentrash <startoflateness@gmail.com>
2021-02-07 10:26:25 +0900

    add comment

--- api/test/graphql.spec.ts
+++ api/test/graphql.spec.ts
@@ -3,6 +3,7 @@ import { handler } from '../graphql';
 
 describe('Netlify Function: graphql', () => {
   it('Netlify Functions should export handler', () => {
+    // Neltify Functions should export handler!
     expect(handler).to.exist;
   });
 });
