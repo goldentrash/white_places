49d9e59faababd6fbb4ee8f4486d9aa13fafded0
goldentrash <startoflateness@gmail.com>
2021-03-21 11:37:29 +0900

    db setup, dotenv

--- src/functions/graphql.ts
+++ src/functions/graphql.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from 'apollo-server-lambda';
 import resolvers from './resolver';
 import typeDefs from './typeDefs';
+import 'dotenv/config';
 
 const server = new ApolloServer({
   typeDefs,
