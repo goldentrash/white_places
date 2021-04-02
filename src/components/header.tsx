5bfe81959806200c2f3e537b8b71282d79879408
goldentrash <startoflateness@gmail.com>
2021-04-03 03:38:10 +0900

    header styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,86 +1,56 @@
-import React, { ReactElement, FormEventHandler } from 'react';
+import React, { ReactElement } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Link from '@material-ui/core/Link';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    searchBox: {
-      display: 'flex',
-      alignItems: 'center',
+    toolbar: {
+      justifyContent: 'space-between',
     },
-    searchBoxInput: {
-      marginLeft: theme.spacing(1),
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
+    brandLink: {
+      flex: 'none',
+      margin: theme.spacing(1, 1.5),
+      '&:hover': {
+        textDecoration: 'none',
       },
     },
-    searchBoxIconButton: {
-      padding: 10,
-    },
-    link: {
-      margin: theme.spacing(1, 1.5),
-    },
-    grow: {
-      flexGrow: 1,
+    menuLink: {
+      padding: theme.spacing(0.5, 1.5),
     },
   })
 );
 
 const Header = (): ReactElement => {
-  const searchProject: FormEventHandler<HTMLDivElement> = (event) => {
-    event.preventDefault();
-
-    console.log('hello');
-  };
-
   const classes = useStyles();
+
   return (
-    <AppBar position="static" color="default" variant="outlined">
-      <Toolbar>
-        <Button
-          color="primary"
-          size="small"
-          variant="text"
+    <AppBar position="static" color="inherit" elevation={0}>
+      <Toolbar variant="dense" className={classes.toolbar}>
+        <Link
+          variant="h6"
           component={RouterLink}
           to="/"
-          classes={{ root: classes.link }}
+          className={classes.brandLink}
         >
           White Places
-        </Button>
-        <div className={classes.grow} />
-        <Paper
-          component="form"
-          onSubmit={searchProject}
-          variant="outlined"
-          className={classes.searchBox}
-        >
-          <InputBase
-            color="primary"
-            placeholder="Search Project"
-            type="search"
-            className={classes.searchBoxInput}
-          />
-          <IconButton type="submit" className={classes.searchBoxIconButton}>
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-        <Button
-          href="/"
-          size="small"
-          color="primary"
-          variant="contained"
-          className={classes.link}
-        >
-          Log In?
-        </Button>
+        </Link>
+        <ButtonGroup color="primary" disableElevation variant="outlined">
+          <Button
+            component={RouterLink}
+            to="/search"
+            className={classes.menuLink}
+          >
+            Search
+          </Button>
+          <Button href="/" className={classes.menuLink}>
+            Log In
+          </Button>
+        </ButtonGroup>
       </Toolbar>
     </AppBar>
   );
