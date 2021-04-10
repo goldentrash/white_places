32954afbff3db72f9997f081884a306076192a01
goldentrash <startoflateness@gmail.com>
2021-04-10 20:56:57 +0900

    no waring allowed

--- functions/fauna.ts
+++ functions/fauna.ts
@@ -7,7 +7,8 @@ if (!process.env.FAUNA_SECRET) {
 console.log(`fauna secret: ${process.env.FAUNA_SECRET}`);
 const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET });
 
-const createCollection = async (): Promise<void> => {
+// 관련 파일들만 따로 만들어야 할지도?
+export const createCollection = async (): Promise<void> => {
   await client.query(query.CreateCollection({ name: 'projects' }));
 };
 
